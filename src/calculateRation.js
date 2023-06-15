336c66ce4f6cf655f996b042c389563fa478234a
SergeyRevyagin <west1995@mail.ru>
2023-06-15 19:54:59 +0300

    test for Artem

--- src/calculateRation.js
+++ src/calculateRation.js
@@ -1,23 +1,68 @@
 import getFoodData from './utilities/getFoodData.js';
 
+// функция для рандомного выбора продукта
+const getRandomFood = (type, foodList) => {
+  const keys = Object.keys(foodList);
+  const randomKey = keys[Math.floor(Math.random() * keys.length)];
+  return { food: randomKey, amount: foodList[randomKey][type] };
+};
+
+// функция для расчета БЖУ
 const getFoodCount = (type, count, foodList) => {
-  const result = Object.keys(getFoodData()[type]).reduce((acc, food) => {
-    acc[food] = Math.round((count * 100) / foodList[food][type]);
-    return acc;
-  }, {});
+  const result = {};
+  const units = {};
+
+  // получаем рандомный продукт
+  const randomFood = getRandomFood(type, foodList);
+
+  // считаем количество случайного продукта, для нужного количества БЖУ
+  const requiredAmount = (count * 100) / randomFood.amount;
+  result[randomFood.food] = requiredAmount;
+  units[randomFood.food] = 100;
+
+  // учитываем наличия БЖУ в других продуктах
+  const keys = Object.keys(foodList);
+  keys.filter((otherFood) => !otherFood.includes(randomFood.food)).forEach((otherFood) => {
+    const requiredOtherAmount = (requiredAmount * foodList[otherFood][type]) / 100;
+    result[randomFood.food] += requiredOtherAmount;
+    units[randomFood.food] += 100;
+  });
+
+  // считаем дополнительное количество продукта, если необходимо  БЖУ из других продуктов
+  keys.forEach((food) => {
+    const foodUnit = units[food];
+    const foodGrams = foodUnit / 100;
+    keys.filter((otherFood) => !otherFood.includes(food)).forEach((otherFood) => {
+      const otherFoodGrams = (foodGrams * foodList[otherFood][type]) / 100;
+      if (otherFoodGrams > foodList[otherFood][type]) {
+        const additionAmount = (otherFoodGrams - foodList[otherFood][type]) / foodList[food][type];
+        result[food] += additionAmount * foodGrams;
+      }
+    });
+  });
+
+  // оставляем только необходимое количества продукта
+  keys.forEach((food) => {
+    result[food] = Math.floor((result[food] / units[food]) * 100);
+  });
+
   return result;
 };
 
-const calculateRation = (calories) => {
+// Основная функция для получения массива продуктов на 7 дней
+const calculateRation = (calories, days = 7) => {
   const { protein, fat, carbs } = calories;
-  const proteinFood = getFoodCount('protein', protein, getFoodData().protein);
-  const fatFood = getFoodCount('fat', fat, getFoodData().fat);
-  const carbsFood = getFoodCount('carbs', carbs, getFoodData().carbs);
-  const result = {
-    proteinFood,
-    fatFood,
-    carbsFood,
-  };
+  const result = {};
+  for (let i = 0; i < days; i += 1) {
+    const proteinFood = getFoodCount('protein', protein, getFoodData().protein);
+    const fatFood = getFoodCount('fat', fat, getFoodData().fat);
+    const carbsFood = getFoodCount('carbs', carbs, getFoodData().carbs);
+    result[i] = {
+      proteinFood,
+      fatFood,
+      carbsFood,
+    };
+  }
   return result;
 };
 
