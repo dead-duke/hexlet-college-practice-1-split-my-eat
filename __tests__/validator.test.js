ad76179786e3f8a5a292b75ad65e395afbdfeab4
SergeyRevyagin <west1995@mail.ru>
2023-06-28 22:40:37 +0300

    add validator.test.js

--- __tests__/validator.test.js
+++ __tests__/validator.test.js
@@ -1,23 +1,23 @@
 import validator from '../src/utilities/validator.js';
 
-  test('should return true for a valid input', () => {
-    const input = '10';
-    const type = 'number';
-    const min = 0;
-    const max = 100;
-    
-    const result = validator(input, type, min, max);
-    
-    expect(result).toBe(true);
-  });
+test('should return true for a valid input', () => {
+  const input = '10';
+  const type = 'number';
+  const min = 0;
+  const max = 100;
 
-  test('should return an error message for an invalid input', () => {
-    const input = 'abc';
-    const type = 'number';
-    const min = 0;
-    const max = 100;
-    
-    const result = validator(input, type, min, max);
-    
-    expect(result).toBe(`Please enter a valid ${type}, min. ${type} ${min}, max. ${type} ${max}`);
-  });
+  const result = validator(input, type, min, max);
+
+  expect(result).toBe(true);
+});
+
+test('should return an error message for an invalid input', () => {
+  const input = 'abc';
+  const type = 'number';
+  const min = 0;
+  const max = 100;
+
+  const result = validator(input, type, min, max);
+
+  expect(result).toBe(`Please enter a valid ${type}, min. ${type} ${min}, max. ${type} ${max}`);
+});
